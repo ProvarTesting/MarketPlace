06b025964c2ca2c991a39583597b9f0c49429936
sudarshan128 <sudarshan.singh@provartesting.com>
2020-08-05 13:29:29 +0530

    This API returns random number between given Range

---
+++ GenerateRandomNum.java
@@ -0,0 +1,79 @@
+package customapis;
+
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+import com.provar.core.model.base.api.ValueScope;
+import com.provar.core.testapi.ITestExecutionContext;
+import com.provar.core.testapi.annotations.*;
+
+@TestApi( title="Generate Random Num"
+        , summary=""
+        , remarks=""
+        , iconBase=""
+        , defaultApiGroups={"Custom"}
+        )
+@TestApiParameterGroups(parameterGroups={
+	    @TestApiParameterGroup(groupName="inputs", title="Inputs"),
+	    @TestApiParameterGroup(groupName="result", title="Result"),
+	    })
+public class GenerateRandomNum {
+    
+    @TestApiParameter(seq=1, 
+            summary="The first parameter's summary.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="inputs")
+    public int StartingNum;
+    
+    @TestApiParameter(seq=2, 
+            summary="The second parameter's summary.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="inputs")
+    public int EndingNum;
+
+    @TestApiParameter(seq=10, 
+            summary="The name that the result will be stored under.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="result")
+    public String resultName;
+
+    @TestApiParameter(seq=11, 
+            summary="The lifespan of the result.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="result",
+            defaultValue="Test")
+    public ValueScope resultScope;
+
+    /** 
+     * Used to write to the test execution log.
+     */
+    @TestLogger
+    public Logger testLogger;
+    
+    /** 
+     * Provides access to facilities, mainly to set and get variable values.
+     */
+    @TestExecutionContext
+    public ITestExecutionContext testExecutionContext;
+    
+    @TestApiExecutor
+    public void execute() {
+
+    	// Put our implementation logic here.
+    	testLogger.info("Hello from " + this.getClass().getName());
+    	
+    	Random r = new Random();
+    	int result = r.nextInt(EndingNum-StartingNum) + StartingNum;
+
+        // Store the result (if appropriate).
+    	int dummyResult = result;
+        testExecutionContext.setValue(resultName, dummyResult, resultScope);
+        
+    }
+    
+}
