72dc49d6a37c507c639edfa2ccbaa096c30b21d7
sudarshan128 <sudarshan.singh@provartesting.com>
2020-09-29 16:27:15 +0530

    This API extract all urls from given string

---
+++ UrlExtractor.java
@@ -0,0 +1,86 @@
+package customapis;
+
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.provar.core.model.base.api.ValueScope;
+import com.provar.core.testapi.ITestExecutionContext;
+import com.provar.core.testapi.annotations.*;
+
+@TestApi( title="Url Extractor"
+        , summary=""
+        , remarks=""
+        , iconBase=""
+        , defaultApiGroups={"Custom"}
+        )
+@TestApiParameterGroups(parameterGroups={
+	    @TestApiParameterGroup(groupName="inputs", title="Inputs"),
+	    @TestApiParameterGroup(groupName="result", title="Result"),
+	    })
+public class UrlExtractor {
+    
+    @TestApiParameter(seq=1, 
+            summary="The first parameter's summary.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="inputs")
+    public String InputText;
+    
+
+    @TestApiParameter(seq=10, 
+            summary="The name that the result will be stored under.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="result")
+    public String resultName;
+
+    @TestApiParameter(seq=11, 
+            summary="The lifespan of the result.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="result",
+            defaultValue="Test")
+    public ValueScope resultScope;
+
+    /** 
+     * Used to write to the test execution log.
+     */
+    @TestLogger
+    public Logger testLogger;
+    
+    /** 
+     * Provides access to facilities, mainly to set and get variable values.
+     */
+    @TestExecutionContext
+    public ITestExecutionContext testExecutionContext;
+    
+    @TestApiExecutor
+    public void execute() {
+
+    	// Put our implementation logic here.
+    	
+    	ArrayList links = new ArrayList();
+		String regex = "\\(?\\b(http://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(InputText);
+		while(m.find()) {
+		String urlStr = m.group();
+		if (urlStr.startsWith("(") && urlStr.endsWith(")"))
+		{
+		urlStr = urlStr.substring(1, urlStr.length() - 1);
+		}
+		links.add(urlStr);
+		}
+    	
+    	testLogger.info("All Links -  "  + links);
+
+        // Store the result (if appropriate).
+    	ArrayList dummyResult = links;
+        testExecutionContext.setValue(resultName, dummyResult, resultScope);
+        
+    }
+    
+}
